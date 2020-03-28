baf05d2d897314338151de383c758b6f83d4d9c0
Manuel <manuel.geissbuehler@students.fhnw.ch>
2020-03-28 18:05:10 +0100

    solved AB1

--- src/main/java/ch/fhnw/module06/ab1/ApplicationUI.java
+++ src/main/java/ch/fhnw/module06/ab1/ApplicationUI.java
@@ -1,4 +1,23 @@
 package ch.fhnw.module06.ab1;
 
-public class ApplicationUI {
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class ApplicationUI extends StackPane {
+    private Button button;
+    
+    public ApplicationUI() {
+        initializeControls();
+        layoutControls();
+    }
+
+    private void layoutControls() {
+        getChildren().add(button);
+    }
+
+    private void initializeControls() {
+        button = new Button("I love you my dear");
+    }
+
+
 }
--- src/main/java/ch/fhnw/module06/ab1/Starter.java
+++ src/main/java/ch/fhnw/module06/ab1/Starter.java
@@ -1,4 +1,29 @@
 package ch.fhnw.module06.ab1;
 
-public class Starter {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent rootPanel = new ApplicationUI();
+
+        Scene scene = new Scene(rootPanel);
+
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(300);
+        primaryStage.centerOnScreen();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
